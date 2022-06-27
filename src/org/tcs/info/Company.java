131fb24787599f2793d300abb8ba3ee66535b6db
dhanapriyacgitacc <dhanapriyac90@gmal.com>
2022-06-27 11:23:49 -0700

    Branch2

--- src/org/tcs/info/Company.java
+++ src/org/tcs/info/Company.java
@@ -8,12 +8,17 @@ public class Company {
 	public void cmpName() {
 		System.out.println("Company name : TCS");
 	}
+	
+	private void cmpSoftware() {
+		System.out.println("Company Software : Eclipse");
+	}
 	public static void main(String[] args) {
 		
 		Company c = new Company();
 		EmployeeInformation e = new EmployeeInformation();
 		c.cmpID();
 		c.cmpName();
+		c.cmpSoftware();
 		e.empID();												//To Method call	
 		e.empName();
 		e.empDob();
